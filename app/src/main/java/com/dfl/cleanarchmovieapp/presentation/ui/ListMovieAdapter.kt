720dcc62492869842c7e829bc8797bcf0dce9871
dellylucas <dellylucas@hotmail.com>
2021-06-15 09:40:04 -0500

    adaptador de lista  de peliculas a mostrar

---
+++ app/src/main/java/com/dfl/cleanarchmovieapp/presentation/ui/ListMovieAdapter.kt
@@ -0,0 +1,42 @@
+package com.dfl.cleanarchmovieapp.presentation.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.dfl.cleanarchmovieapp.databinding.AdapterListMovieBinding
+import com.dfl.cleanarchmovieapp.domain.model.Movie
+
+class ListMovieAdapter :
+    ListAdapter<Movie, ListMovieAdapter.ListViewHolder>(DifUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val itemBinding =
+            AdapterListMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val paymentBean = getItem(position)
+        holder.bind(paymentBean)
+    }
+
+    inner class ListViewHolder(private val view: AdapterListMovieBinding) :
+        RecyclerView.ViewHolder(view.root) {
+
+        fun bind(movie: Movie) {
+            view.movieTextView.text = movie.name
+        }
+
+    }
+}
+
+private class DifUtil : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+        oldItem == newItem
+
+}
--- app/src/main/java/com/dfl/cleanarchmovieapp/presentation/ui/ListMovieFragment.kt
+++ app/src/main/java/com/dfl/cleanarchmovieapp/presentation/ui/ListMovieFragment.kt
@@ -1,14 +1,13 @@
 package com.dfl.cleanarchmovieapp.presentation.ui
 
-import android.R
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dfl.cleanarchmovieapp.databinding.FragmentListMovieBinding
 import com.dfl.cleanarchmovieapp.presentation.vm.ManagementMoviesVM
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,6 +20,7 @@ class ListMovieFragment : Fragment() {
 
     private val binding get() = _binding!!
     private val viewModel: ManagementMoviesVM by activityViewModels()
+    private val adapter = ListMovieAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,17 +32,17 @@ class ListMovieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.moviesRecyclerView.apply {
+            adapter = adapter
+            layoutManager = GridLayoutManager(requireContext(), 3)
+        }
         viewModel.getAllMovies()
         viewModel.movies.observe(viewLifecycleOwner, { model ->
 
             when (model) {
                 is UiModel.Loading -> Log.d("test", "loading")
-                is UiModel.ContentMovies -> {
-                    binding.productsRecyclerView.adapter= ArrayAdapter(requireContext(), R.layout.simple_list_item_1, model.movies.map { ut->ut.name });
-                    model.movies.forEach {
-                        Log.d("test", "movie ${it.name}")
-                    }
-                }
+                is UiModel.ContentMovies -> adapter.submitList(model.movies)
                 else -> Log.d("test", "nothing")
             }
         })
